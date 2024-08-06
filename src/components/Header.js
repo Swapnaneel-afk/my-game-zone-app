6c014f83c321ac43c6ad2f4a126d6df5e5044cdd
Dream_Blue <82110721+Swapnaneel-afk@users.noreply.github.com>
2024-08-07 00:10:44 +0530

    Did some Styling idk

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = ({ onStartPlaying }) => (
-  <header>
+  <header className="welcome-header">
     <h1>Welcome to GameZone</h1>
     <p>Play your favorite games and challenge your friends!</p>
     <button onClick={onStartPlaying}>Start Playing</button>
